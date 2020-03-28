17667f4c11a5b48d3afe2f25e04fd4d7d0694750
stereoIII6 <stereodocbush@gmail.com>
2020-03-28 14:29:01 +0100

    init

---
+++ src/comps/topMenu.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from "reactstrap";
+const TopMenu = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="light" light expand="md">
+      <Container>
+        <NavbarBrand href="/">
+          <img src="logo.png" style={{ width: "80px" }} alt="" />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Die Duftmanufaktur</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="http://scentral.herokuapp.com" target="_blank">
+                Scentral
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Kontakt
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Rufen Sie Uns an !</DropdownItem>
+                <DropdownItem>Schreiben Sie uns per WhatsApp</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Schreiben Sie uns eine eMail</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>
+            <i className="fas fa-shopping-cart"></i>
+          </NavbarText>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopMenu;
